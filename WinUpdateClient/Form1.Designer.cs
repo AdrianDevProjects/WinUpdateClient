621f577012b5e0f761c7cf6d414d9ea9302d319c
Adrian Albrecht <albrechtadrian08@gmail.com>
2023-01-29 15:22:08 +0100

    Projektdateien hinzufügen.

---
+++ WinUpdateClient/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WinUpdateClient
+{
+    partial class download_window
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.download_label = new System.Windows.Forms.Label();
+            this.download_progress = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // download_label
+            // 
+            this.download_label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.download_label.AutoSize = true;
+            this.download_label.Location = new System.Drawing.Point(353, 185);
+            this.download_label.Name = "download_label";
+            this.download_label.Size = new System.Drawing.Size(79, 13);
+            this.download_label.TabIndex = 0;
+            this.download_label.Text = "downloadLabel";
+            this.download_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // download_progress
+            // 
+            this.download_progress.Location = new System.Drawing.Point(178, 201);
+            this.download_progress.Name = "download_progress";
+            this.download_progress.Size = new System.Drawing.Size(426, 23);
+            this.download_progress.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.download_progress.TabIndex = 1;
+            // 
+            // download_window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.download_progress);
+            this.Controls.Add(this.download_label);
+            this.Name = "download_window";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "WinUpdateClient";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.download_window_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label download_label;
+        private System.Windows.Forms.ProgressBar download_progress;
+    }
+}
+
---
+++ WinUpdateClient/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Net;
+using System.IO.Compression;
+using System.IO;
+
+namespace WinUpdateClient
+{
+    public partial class download_window : Form
+    {
+        public download_window()
+        {
+            InitializeComponent();
+        }
+        static void ExtractDownloadedZIP()
+        {
+            string zipPath = @"C:\AdrianDevProjects\Newest.zip";
+            string extractPath = @"C:\AdrianDevProjects\UpdatedClient";
+
+            if (Directory.Exists(@"C:\AdrianDevProjects\UpdatedClient"))
+            {
+                Directory.Delete(@"C:\AdrianDevprojects\UpdatedClient", true);
+            }
+            System.IO.Compression.ZipFile.ExtractToDirectory(zipPath, extractPath);
+        }
+
+        private void download_window_Load(object sender, EventArgs e)
+        {
+            startDownload();
+        }
+        private void startDownload()
+        {
+            Thread thread = new Thread(() => {
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+                client.DownloadFileAsync(new Uri("https://adriandevprojects.com/TurniereUndEventsClient-master.zip"), @"C:\AdrianDevProjects\Newest.zip");
+            });
+            thread.Start();
+        }
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate {
+                double bytesIn = double.Parse(e.BytesReceived.ToString());
+                double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+                double percentage = bytesIn / totalBytes * 100;
+                download_label.Text = "Downloaded " + e.BytesReceived + "KB of " + e.TotalBytesToReceive + "KB";
+                download_progress.Value = int.Parse(Math.Truncate(percentage).ToString());
+            });
+        }
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate {
+                download_label.Text = "Completed";
+            });
+            ExtractDownloadedZIP();
+            File.Delete(@"C:\AdrianDevProjects\Newest.zip");
+        }
+    }
+}
